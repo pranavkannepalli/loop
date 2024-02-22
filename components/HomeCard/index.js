306e6366a4151cb4a76b1dde0a2ea7e7ab2eb983
PranavK226 <pranav.kannepalli@gmail.com>
2024-02-21 17:27:47 -0800

    debugging w/classNames

--- components/HomeCard/index.js
+++ components/HomeCard/index.js
@@ -3,16 +3,18 @@
 import Icon from "../Icon";
 import { useContext } from "react";
 import LoopContext from "../LoopContext";
+import classNames from "@/hooks/classnames";
 
 export default function HomeCard({ svg, color, borderColor, name, description }) {
 	const { userData, changeHomeType } = useContext(LoopContext);
 
 	return (
 		<div
-			className={
-				`box-border flex flex-col gap-[5px] flex-1 p-[10px] sm:p-[20px] items-center justify-center ${color} rounded-[20px] aspect-[4/3] hover:border-[3px] hover:border-solid ${"hover:" + borderColor} transition-all` +
-				(userData.hometype == name ? ` border-solid border-[3px] ${borderColor}` : "")
-			}
+			className={classNames(
+				`box-border flex flex-col gap-[5px] flex-1 p-[10px] sm:p-[20px] items-center justify-center ${color} rounded-[20px] aspect-[4/3] hover:border-[3px] hover:border-solid transition-all`,
+				`hover:!${borderColor}`,
+				userData.hometype == name ? ` border-solid border-[3px] ${borderColor}` : ""
+			)}
 			onClick={() => {
 				changeHomeType(name);
 			}}
