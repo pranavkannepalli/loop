3078b3a084673e669c726a167a23602f163d861d
Achintya72 <achintya.agrawal.90@gmail.com>
2024-02-24 17:04:04 -0800

    Made priority card draggable

--- components/PrioritiesReorder/index.js
+++ components/PrioritiesReorder/index.js
@@ -9,7 +9,7 @@ export default function PrioritiesReorder({}) {
 	const { userData, changePriorities } = useContext(LoopContext);
 
 	return (
-		<Reorder.Group axis="y" className="flex flex-col h-full gap-[20px]" onReorder={(newOrder) => changePriorities(newOrder)} values={userData.priorities}>
+		<Reorder.Group axis="y" className="flex-1 flex flex-col h-full gap-[20px]" onReorder={(newOrder) => changePriorities(newOrder)} values={userData.priorities}>
 			{userData.priorities.map((item) => (
 				<Priority item={item} key={item} />
 			))}
@@ -20,37 +20,31 @@ export default function PrioritiesReorder({}) {
 let itemDetails = {
 	"Cutting Down on Bills": {
 		color: "bg-blue-500",
+		iconColor: "text-blue-700",
 		icon: "money",
 	},
 	"Lowering my Footprint": {
 		color: "bg-green-500",
+		iconColor: "text-green-700",
 		icon: "leaf",
 	},
 	"Becoming Self-Sufficient": {
 		color: "bg-purple-500",
+		iconColor: "text-purple-700",
 		icon: "independent",
 	},
 };
 
 const Priority = ({ item }) => {
-	const controls = useDragControls();
-
 	return (
-		<Reorder.Item value={item} dragListener={false} dragControls={controls}>
-			<div className={`rounded-[20px] ${itemDetails[item].color} p-[16px] pr-[32px] flex flex-row items-center justify-between`}>
+		<Reorder.Item value={item}>
+			<div className={`rounded-[20px] ${itemDetails[item].color} p-[16px] pr-[32px] flex flex-row items-center justify-between cursor-grabbing`}>
 				<div className="flex flex-row items-center gap-[5px]">
-					<Icon name={itemDetails[item].icon} />
-					<p className="font-krona uppercase">{item}</p>
+					<Icon name={itemDetails[item].icon} size={20} className={itemDetails[item].iconColor}/>
+					<p className="font-krona uppercase text-white-600">{item}</p>
 				</div>
-				<div
-					style={{ touchAction: "none" }}
-					onPointerDown={(e) => {
-						console.log("Dragging");
-						controls.start(e);
-					}}
-					className="cursor-grabbing"
-				>
-					<Icon name="grab" />
+				<div>
+					<Icon name="grab" size={20} className={itemDetails[item].iconColor} />
 				</div>
 			</div>
 		</Reorder.Item>
