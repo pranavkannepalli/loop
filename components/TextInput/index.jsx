dd43197bb82b8330d72dc4b16f5c115c3737bf53
Achintya72 <achintya.agrawal.90@gmail.com>
2024-02-24 17:03:23 -0800

    Tried to Fix Input Component

--- components/TextInput/index.jsx
+++ components/TextInput/index.jsx
@@ -2,12 +2,12 @@ import classNames from "@/hooks/classnames"
 
 export default function TextInput({ size="small", hasInput=true, prefix=null, suffix = null, ...props}) {
     return (
-        <div className={classNames(`flex gap-[16px] items-center 
+        <div className={classNames(`flex flex-1 gap-[16px] items-center 
                         px-[16px]
                         outline outline-2 outline-white-300
                         hover:outline-white-400 focus-within:outline-green-700`, size=="small" ? "py-[12px] rounded-[8px] bg-white-100" : "p-[16px] rounded-[16px] bg-transparent")}>
             {prefix}
-            {hasInput && <input className="focus:outline-none !bg-transparent font-krona" {...props}/>}
+            {hasInput && <input className=" flex-[10] focus:outline-none !bg-transparent font-krona" {...props}/>}
             {suffix}
         </div>
     )
