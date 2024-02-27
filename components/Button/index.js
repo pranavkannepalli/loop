176be581aa50e6411838b8c5755c7f4e8fa5a2e4
Achintya72 <achintya.agrawal.90@gmail.com>
2024-02-26 23:50:05 -0800

    Added prefixes to buttons

--- components/Button/index.js
+++ components/Button/index.js
@@ -2,9 +2,10 @@
 
 import classNames from "@/hooks/classnames";
 
-export default function Button({ type = "primary", children, className = "", onClick = () => {} }) {
+export default function Button({ prefix=null, type = "primary", children, className = "", onClick = () => {} }) {
 	return (
-		<button className={classNames("px-[32px] py-[16px] rounded-[16px]", type == "primary" ? "bg-green-800 text-white-200" : "bg-none text-green-700", className)} onClick={onClick}>
+		<button className={classNames("px-[32px] flex gap-[10px] items-center justify-center py-[16px] rounded-[16px]", type == "primary" ? "bg-green-800 text-white-200" : "bg-none text-green-700", className)} onClick={onClick}>
+			{prefix}
 			{children}
 		</button>
 	);
