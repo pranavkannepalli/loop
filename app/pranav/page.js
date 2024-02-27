7e057635eedb9eb4b77c0f8adcb696fc55e7715a
Achintya72 <achintya.agrawal.90@gmail.com>
2024-02-26 23:49:23 -0800

    Added Graphs

--- app/pranav/page.js
+++ app/pranav/page.js
@@ -1,7 +1,21 @@
+import CostGraph from "@/components/CostGraph";
 import Dropdown from "@/components/Dropdown";
 import Icon from "@/components/Icon";
 import PrioritiesReorder from "@/components/PrioritiesReorder";
+import ROIGraph from "@/components/ROIGraph";
 import TextInput from "@/components/TextInput";
+
+const points = [
+    [1985, 30000],
+    [1990, 18000],
+    [1995, 16000],
+    [2000, 13500],
+    [2005, 13000],
+    [2010, 11500],
+    [2015, 10500],
+    [2020, 10000],
+    [2025, 5000],
+];
 
 export default function Pranav() {
 	return (
@@ -20,6 +34,8 @@ export default function Pranav() {
 			<Dropdown items={["Hi", "bye", "cya"]} />
 			<TextInput prefix={<Icon name="add" size={20} />} suffix={<Dropdown items={["Hi", "bye", "cya"]} />} placeholder="First Name" size="large" />
 			<PrioritiesReorder />
+			<CostGraph points={points} t={2000}/>
+			<ROIGraph points={points} t={2000}/>
 		</div>
 	);
 }
