7bbdee7185edc807670e165f7d6b2b6808dd11a6
PranavK226 <pranav.kannepalli@gmail.com>
2024-02-29 20:18:37 -0800

    about and sources

---
+++ app/about/page.js
@@ -0,0 +1,24 @@
+import Link from "next/link";
+
+export default function About({}) {
+	return (
+		<main>
+			<section className="flex flex-col box-border gap-[20px] !mt-[100px] layout">
+				<h4>About Us</h4>
+				<p>
+					To ensure a seamless, fast, and optimized user experience, this website uses Vercel&apos;s NextJS framework, a ReactJS technology as the coding technology. However, the design
+					system and brand themes are all original and specially designed with the user&apos;s experience and accessibility in mind.
+				</p>
+				<p>All icons were created by the Loop team.</p>
+				<div className="flex flex-col">
+					<Link href="/CopyrightChecklist.pdf" locale={false} target="_blank">
+						Copyright Checklist (Click to open)
+					</Link>
+					<Link href="/SignedWorkLog.pdf" locale={false} target="_blank">
+						Signed Plan of Work Log (Click to open)
+					</Link>
+				</div>
+			</section>
+		</main>
+	);
+}
--- app/sources/page.js
+++ app/sources/page.js
@@ -1,10 +1,20 @@
 import sources from "./sources";
+import Link from "next/link";
 
 export default function Sources() {
 	return (
 		<main>
-			<section className="flex flex-col box-border items-center mt-[100px]">
-				<h4>Sources Page</h4>
+			<section className="flex flex-col gap-[20px] box-border !mt-[100px] layout">
+				<h4>Important Documents</h4>
+				<div className="flex flex-col">
+					<Link href="/CopyrightChecklist.pdf" locale={false} target="_blank">
+						Copyright Checklist (Click to open)
+					</Link>
+					<Link href="/SignedWorkLog.pdf" locale={false} target="_blank">
+						Signed Plan of Work Log (Click to open)
+					</Link>
+				</div>
+				<h4>Sources</h4>
 				{sources.map((val, index) => (
 					<a href={val} key={index}>
 						{val}
