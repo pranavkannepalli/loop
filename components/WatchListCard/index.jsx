68e4b9f03bf1a1e432a2a0ccafaf8f0ab7374b3b
Achintya72 <achintya.agrawal.90@gmail.com>
2024-02-29 16:51:16 -0800

    Created trend indicator component

---
+++ components/WatchListCard/index.jsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import TrendIndicator from "../TrendIndicator";
+
+export default function WatchlistCard({ img, alt, name, price, trend, remove }) {
+    return (
+        <div className="flex items-stretch bg-white-100 border border-white-300  overflow-hidden w-max rounded-[8px]">
+            <div className="relative w-[80px]">
+                <Image src={img} alt={alt} fill />
+            </div>
+            <div className="flex flex-col p-[16px] gap-[5px]">
+                <h6>{name}</h6>
+                <div className="flex justify-between gap-[10px] items-center">
+                    <p className="caption text-white-400">${price.toLocaleString()}</p>
+                    <TrendIndicator trend={trend} />
+                </div>
+            </div>
+            <p onClick={remove} className="caption p-[10px] flex justify-content items-center bg-white-200">-</p>
+        </div>
+    )
+}
