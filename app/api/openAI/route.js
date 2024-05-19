3e18f4868551002204d2f15010e15494b326f3ca
Pranav Kannepalli <pranav.kannepalli@gmail.com>
2024-05-19 11:24:11 -0700

    fixed openAI

--- app/api/openAI/route.js
+++ app/api/openAI/route.js
@@ -1,22 +1,34 @@
 import { NextRequest, NextResponse } from "next/server";
 import OpenAI from "openai";
+import solutions from "../../solutions";
 
-export async function GET(request) {
+export async function POST(request) {
 	try {
+		var requestData = await request.json();
+
+		var state = requestData["state"];
+		var s = [];
+
+		for (const element of solutions[state]) {
+			s.push(element.title);
+		}
+
+		var query = requestData["query"];
+
 		const openai = new OpenAI();
 		const completion = await openai.chat.completions.create({
 			messages: [
-				{ role: "system", content: "You are a helpful assistant." },
-				{ role: "user", content: "Who won the world series in 2020?" },
-				{ role: "assistant", content: "The Los Angeles Dodgers won the World Series in 2020." },
-				{ role: "user", content: "Where was it played?" },
+				{
+					role: "system",
+					content: `You are a helpful assistant whose job is to pick solutions that best match the user's query.  Only pick solutions from this list: ${s}. Only return applicable solutions. Include no other text and separate them by commas.`,
+				},
+				{ role: "user", content: query },
 			],
 			model: "gpt-3.5-turbo",
 		});
 
-		var complete = completion.choices[0];
+		var complete = completion.choices[0]["message"]["content"];
 
-		console.log(complete);
 		return NextResponse.json({ message: complete }, { status: 200 });
 	} catch (err) {
 		console.log(err);
